dcfe0303b0779c03c9d03ac9b388a95afe163548
Aleksey Vokhmin <vohmina2011@yandex.ru>
2024-12-04 16:19:17 +0300

    Add map component and integrate with offers list; refactor related types and hooks

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const MARKERS = {
+  URL_MARKER_DEFAULT:
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg',
+  URL_MARKER_CURRENT:
+    'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg',
+} as const;
