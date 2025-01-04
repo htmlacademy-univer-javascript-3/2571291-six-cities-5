bdfdb1ad86508dc5f6bccc6f8b97fde87d16395b
Aleksey Vokhmin <vohmina2011@yandex.ru>
2025-01-04 19:40:01 +0300

    Update map component icons and improve marker selection logic

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,6 @@
 export const MarkersIcons = {
-  URL_MARKER_DEFAULT:
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg',
-  URL_MARKER_CURRENT:
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg',
+  URL_MARKER_DEFAULT: '/img/pin.svg',
+  URL_MARKER_CURRENT: '/img/pin-active.svg',
 } as const;
 
 export const CityLocations = {
